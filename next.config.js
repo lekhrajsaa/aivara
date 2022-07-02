ffabe8a87ef099331e8784bac7660df59b2880e1
lekhrajsaini0 <lekhrajsaini700@gmail.com>
2022-07-02 09:25:44 +0530

    last try

--- next.config.js
+++ next.config.js
@@ -9,4 +9,10 @@ module.exports = {
   env: {
     REACT_APP_CLIENT: "http://localhost:3000",
     REACT_APP_SERVER: "http://localhost:5000",
-    },};
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  }
+};
