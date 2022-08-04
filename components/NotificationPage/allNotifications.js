b45d644ab612ee0bf12b49ea9fec77cd19665d77
lekhrajsaini0 <lekhrajsaini700@gmail.com>
2022-08-04 10:32:52 +0530

    updated notification page

--- components/NotificationPage/allNotifications.js
+++ components/NotificationPage/allNotifications.js
@@ -5,7 +5,7 @@ import { setNotification } from '../../redux/dataAction';
 import { useRouter } from 'next/router';
 import { style } from '@mui/system';
 
-const allNotifications = ({ id ,key,data, dateMacker, timeMacker,checkNotificationsHandler}) => {
+const allNotifications = ({ id, key, data, dateMacker, timeMacker, checkNotificationsHandler }) => {
 
   const notifications = useSelector(state => state.userdata.notification);
   const dispatch = useDispatch();
@@ -19,7 +19,7 @@ const allNotifications = ({ id ,key,data, dateMacker, timeMacker,checkNotificati
 
   // console.log('not', data)
   function notificationClickHanlder(id) {
-    
+
     const modNotifications = [...notifications];
     modNotifications.forEach(item => {
       if (item.id === id) {
@@ -33,35 +33,48 @@ const allNotifications = ({ id ,key,data, dateMacker, timeMacker,checkNotificati
     router.push('/reports')
   }
   // console.log("hello",data)
-  
+
   return (
     <>
 
-      <h1 className={classes.notifi_heading}>Notifications 
-        {notifications && !(notifications?.filter(notif => notif.checked === false).length < 1) && <span className={classes.notificationDot} style={{ width: "10px", height: "10px", backgroundColor: "blue", borderRadius: "50%", display: "inline-block" }}></span> }
-</h1>
+      <h1 className={classes.notifi_heading}>Notifications
+        {notifications && !(notifications?.filter(notif => notif.checked === false).length < 1) &&
+         <span className={classes.notificationDot}
+          style={{ width: "10px",
+           height: "10px",
+           position:"relative",
+            backgroundColor: "#3699FB",
+             borderRadius: "50%",
+              display: "inline-block",
+              left:"5px",
+              top:"-6px"
+          }}
+              >
+                </span> 
+               } 
+      </h1>
       <section className={classes.notificationBox} >
-        {/* <div onClick={notificationClickHanlder} className={`${classes.notification} ${readStatus ? ''  : classes.notRead  }`}> */}
-       {
-            data?.length > 0
-              ?
-              data.map((item) => (
-                
-                <div onClick={() => { notificationClickHanlder(item.id)}} className={`${classes.notificationRow} ${item.checked ? '' : classes.notRead}`} id={item.id}>
-                  <div>
-                    <span>{item.clientName}</span><br></br>
-                    <span>{timeMacker(item.customTimeStamp)} ,{dateMacker(item.customTimeStamp)}</span>
-                  </div>
-                  <div>
-                    {item.reportStatus}
-                  </div>
+     
+        {
+          data?.length > 0
+            ?
+            data.map((item) => (
+
+              <div onClick={() => { notificationClickHanlder(item.id) }} className={`${classes.notificationRow} ${item.checked ? '' : classes.notRead}`} id={item.id}>
+                <div>
+                  <span>{item.clientName}</span><br></br>
+                  <span>{timeMacker(item.customTimeStamp)} ,{dateMacker(item.customTimeStamp)}</span>
                 </div>
-                ))
-              :
-              <p style={{ color: '#ccc', textAlign: 'center', margin: '15% 0',fontSize:"35px" }}>No Notifications to show.</p>
-          }
-       
-        {/* </div> */}
+                <div>
+                  {item.reportStatus}
+                </div>
+              </div>
+            ))
+            :
+            <p style={{ color: '#ccc', textAlign: 'center', margin: '15% 0', fontSize: "35px" }}>No Notifications to show.</p>
+        }
+
+     
       </section>
     </>
 
